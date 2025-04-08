7b756b41f0eb7df08b16898950ad621eb4fd3f8b
shanto225 <shantoshibu1@gmail.com>
2025-04-08 12:47:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Show popup
 document.getElementById("contactBtn").addEventListener("click", () => {
-    alert("Thanks for reaching out! Email us at: contact@integratelearning.com");
+    document.getElementById("contactModal").style.display = "block";
   });
+  
+  // Close popup
+  document.getElementById("closeModal").addEventListener("click", () => {
+    document.getElementById("contactModal").style.display = "none";
+  });
+  
+  // Close if user clicks outside the modal
+  window.onclick = function(event) {
+    const modal = document.getElementById("contactModal");
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+  
   
